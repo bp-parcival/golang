044a40657bb5a48270e51eca53d51cd550317f6d
Tokhtaev Buned <so.bonnie@bk.ru>
2025-10-21 01:49:57 +0300

    Homework-2

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello world")
+	var x int
+	fmt.Println("Введите имя: ")
+	fmt.Fscan(os.Stdin, &x)
+	for x < 12307 {
+		if x < 0 {
+			x *= -1
+		}
+		if x%7 == 0 {
+			x *= 39
+		}
+		if x%9 == 0 {
+			x *= 13
+			x += 1
+			fmt.Println(x)
+			continue
+		} else {
+			x += 2
+			x *= 3
+		}
+		fmt.Println(x)
+	}
 }
